54f59385ab6f44c83196a71d5f05ae4736f3f17b
verseler <handuman.verselerkerr@gmail.com>
2023-12-21 08:08:56 +0800

    update: logo and title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import logo from '../assets/logo.svg';
+
 export default function Header() {
   return <header className="flex items-center px-5 bg-yellow-200 h-14">
     <p className="text-lg font-bold">MASTERMIND</p>
