98f4803975dae72ad23334ab2192f34bcf212212
Verseler <handuman.verselerkerr@gmail.com>
2024-01-23 23:35:41 +0800

    chore: convert img element to lazyloadimage

--- src/components/GameGuideDialog.jsx
+++ src/components/GameGuideDialog.jsx
@@ -4,9 +4,10 @@ import decodingboardImg from "../assets/decodingboard.png";
 import scoreImg from "../assets/score.png";
 import secretImg from "../assets/secret.png";
 import winImg from "../assets/win.png";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import Button from "./CTAButton";
 
-export default function GameGuideDialog({onClickAction}) {
+export default function GameGuideDialog({ onClickAction }) {
   return (
     <div className="fixed inset-0 z-30 grid h-full place-content-center bg-black/40">
       <div className="cursor-pointer relative w-screen h-screen overflow-x-scroll text-xl border-4 border-black sm:max-h-[500px] sm:max-w-2xl sm:rounded-xl p-10 bg-amber-200">
@@ -14,7 +15,8 @@ export default function GameGuideDialog({onClickAction}) {
           <span className="fixed material-symbols-outlined">close</span>
         </div>
         <h1 className="mb-8 text-base font-bold text-center sm:text-xl">
-          Welcome to <span className="block text-xl sm:text-3xl">MASTERMIND</span>
+          Welcome to{" "}
+          <span className="block text-xl sm:text-3xl">MASTERMIND</span>
         </h1>
         <ul className="text-base list-disc text-start sm:text-xl">
           <li>
@@ -31,10 +33,7 @@ export default function GameGuideDialog({onClickAction}) {
               <span className="text-nb-red">secret codes</span> or a row of
               boxes with a question mark. Each box contains a hidden color that
               you need to guess.
-              <img
-                className="mt-1 border-2 border-black h-30"
-                src={secretImg}
-              />
+              <LazyLoadImage className="mt-1 border-2 border-black h-30" src={secretImg} alt="boxes with question marks" />
             </li>
           </div>
           <div>
@@ -43,20 +42,14 @@ export default function GameGuideDialog({onClickAction}) {
               <span className="text-nb-blue">color pegs</span> that are located
               at the bottom.
             </li>
-            <img
-              className="mt-1 border-2 border-black h-30"
-              src={codingpegsImg}
-            />
+            <LazyLoadImage className="mt-1 border-2 border-black h-30" src={codingpegsImg} alt="a row of boxes with different colors" />
           </div>
           <div>
             <li>
               After that, on the{" "}
               <span className="text-nb-blue">decoding board</span>, you can
               click on the empty box to make your guess.
-              <img
-                className="mt-1 border-2 border-black h-30"
-                src={decodingboardImg}
-              />
+              <LazyLoadImage className="mt-1 border-2 border-black h-30" src={decodingboardImg} alt="a board of boxes" />
             </li>
           </div>
           <div>
@@ -65,15 +58,18 @@ export default function GameGuideDialog({onClickAction}) {
               to check if your set of guesses is correct. If it is not, then it
               will give a feedback or scores that will be displayed on the
               <span className="text-nb-blue"> scoreboard</span>.
-              <img className="mt-1 border-2 border-black h-30" src={scoreImg} />
+              <LazyLoadImage className="mt-1 border-2 border-black h-30" src={scoreImg} alt="a board of boxes for scores" />
               <p className="block">
-                <span className="font-bold text-black">black</span> - correct color and position.
+                <span className="font-bold text-black">black</span> - correct
+                color and position.
               </p>
               <p className="block">
-                <span className="font-bold text-gray-400">gray</span> - correct color but not the position.
+                <span className="font-bold text-gray-400">gray</span> - correct
+                color but not the position.
               </p>
               <p className="block">
-                <span className="font-bold text-slate-50">white</span> - color and position are wrong.
+                <span className="font-bold text-slate-50">white</span> - color
+                and position are wrong.
               </p>
             </li>
           </div>
@@ -81,23 +77,24 @@ export default function GameGuideDialog({onClickAction}) {
             <li>
               You will be given 10 <span className="text-nb-red">attempts</span>{" "}
               to guess the secret codes.
-              <img
-                className="mt-1 border-2 border-black h-30"
-                src={attemptsImg}
-              />
+              <LazyLoadImage className="mt-1 border-2 border-black h-30" src={attemptsImg} alt="number nine" />
             </li>
           </div>
           <div>
             <li>
               If you guess the secret code before the remaining attempts run
               out, you win the game.
-              <img className="mt-1 border-2 border-black h-30" src={winImg} />
+              <LazyLoadImage className="mt-1 border-2 border-black h-30" src={winImg} alt="winning indicator" />
             </li>
           </div>
         </ol>
 
         <div className="grid w-full mt-8 place-items-center">
-        <Button onClickAction={() => onClickAction} text='CLOSE' bgColor='bg-gray-200' />
+          <Button
+            onClickAction={() => onClickAction}
+            text="CLOSE"
+            bgColor="bg-gray-200"
+          />
         </div>
       </div>
     </div>
