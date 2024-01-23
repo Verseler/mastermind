1a196b81ca0e0337e371e793c381fa7ea7398450
Verseler <handuman.verselerkerr@gmail.com>
2024-01-23 20:45:18 +0800

    chore: disable option button

--- src/pages/TitleScreen.jsx
+++ src/pages/TitleScreen.jsx
@@ -2,13 +2,27 @@ import { Link } from "react-router-dom";
 import NewGameButton from "../components/CTAButton";
 
 export default function TitleScreen() {
+  const tempDisabledOptionBtn = () => {
+    return (
+      <button
+      disabled={true}
+        className={`px-4 py-3 text-sm font-bold text-gray-700 border-2 border-gray-700 opacity-80 rounded-md bg-slate-200  transition-shadow`}
+      >
+        OPTION
+      </button>
+    );
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-6 bg-amber-200">
       <p className="text-3xl font-bold">MASTERMIND</p>
       <div className="flex flex-col gap-2">
-        {/* State management architecture error... */}
-        <Link to="Game"><NewGameButton text={"NEW GAME"} bgColor={'bg-nb-green'} /></Link>
-        <NewGameButton text={"OPTION"} bgColor={'bg-slate-200'} />
+        {/* solve State management architecture error... */}
+        <Link to="Game">
+          <NewGameButton text={"NEW GAME"} bgColor={"bg-nb-green"} />
+        </Link>
+        {tempDisabledOptionBtn()}
+        {/* <NewGameButton text={"OPTION"} bgColor={''} /> */}
       </div>
     </div>
   );
